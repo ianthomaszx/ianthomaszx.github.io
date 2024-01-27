7454a9ebf8290954b7923959bcfe3641beff97a2
ianthomaszx <ianthomaszx@gmail.com>
2024-01-26 21:36:31 -0400

    edit

--- src/js/script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('calculate').addEventListener('click', function () {
-    const os = document.getElementById('os').value * 0.960714;
-    const apt = (document.getElementById('apt').value) * 0.116923;
-    const itens = document.getElementById('itens').value * 0.01375;
-
-    const total = os + apt + itens;
-
-    document.getElementById('total').innerHTML = (total.toFixed(2).replace('.', ',') + "%");
-});
